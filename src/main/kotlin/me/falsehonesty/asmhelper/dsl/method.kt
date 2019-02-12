7e73a69f0f2ab28c27944a0616760ebc0467b5ac
FalseHonesty <thefalsehonesty@gmail.com>
2019-02-12 16:44:40 -0500

    Added overwrite method

--- src/main/kotlin/me/falsehonesty/asmhelper/dsl/method.kt
+++ src/main/kotlin/me/falsehonesty/asmhelper/dsl/method.kt
@@ -3,9 +3,23 @@ package me.falsehonesty.asmhelper.dsl
 
 import me.falsehonesty.asmhelper.AsmHelper
 import me.falsehonesty.asmhelper.dsl.writers.InjectWriter
+import me.falsehonesty.asmhelper.dsl.writers.OverwriteWriter
 
+/**
+ * Injects instructions into the specified place.
+ *
+ * This is a purely additive action and will not remove any existing bytecode.
+ */
 fun inject(config: InjectWriter.Builder.() -> Unit) {
     val writer = InjectWriter.Builder()
+
+    writer.config()
+
+    AsmHelper.asmWriters.add(writer.build())
+}
+
+fun overwrite(config: OverwriteWriter.Builder.() -> Unit) {
+    val writer = OverwriteWriter.Builder()
 
     writer.config()
 
--- src/main/kotlin/me/falsehonesty/asmhelper/dsl/writers/OverwriteWriter.kt
+++ src/main/kotlin/me/falsehonesty/asmhelper/dsl/writers/OverwriteWriter.kt
@@ -1,64 +1,40 @@
 package me.falsehonesty.asmhelper.dsl.writers
 
 import me.falsehonesty.asmhelper.dsl.AsmWriter
-import me.falsehonesty.asmhelper.dsl.At
-import me.falsehonesty.asmhelper.dsl.InjectionPoint
 import net.minecraftforge.fml.common.asm.transformers.deobf.FMLDeobfuscatingRemapper
 import org.objectweb.asm.tree.ClassNode
 import org.objectweb.asm.tree.InsnList
 import org.objectweb.asm.tree.MethodNode
 
-class InjectWriter(
+class OverwriteWriter(
     className: String,
     private val methodName: String,
-    private val at: At,
     private val insnList: InsnList
 ) : AsmWriter(className) {
     override fun transform(classNode: ClassNode) {
         classNode.methods
             .find { FMLDeobfuscatingRemapper.INSTANCE.mapMethodName(classNode.name, it.name, it.desc) == methodName }
-            ?.let { injectInsnList(it) }
+            ?.let { overwriteMethod(it) }
     }
 
-    private fun injectInsnList(method: MethodNode) {
-        var node = when (at.value) {
-            InjectionPoint.HEAD -> method.instructions.first
-            InjectionPoint.TAIL -> method.instructions.last
-        }
-
-        if (at.shift < 0) {
-            repeat(-at.shift) {
-                node = node.previous
-            }
-        } else if (at.shift > 0) {
-            repeat(at.shift) {
-                node = node.next
-            }
-        }
-
-        if (at.before) {
-            method.instructions.insertBefore(node, insnList)
-        } else {
-            method.instructions.insert(node, insnList)
-        }
+    private fun overwriteMethod(node: MethodNode) {
+        node.instructions = insnList
     }
 
     override fun toString(): String {
-        return "AsmWriter{className=$className, methodName=$methodName, at=$at"
+        return "AsmWriter{className=$className, methodName=$methodName"
     }
 
     class Builder {
         var className: String? = null
         var methodName: String? = null
-        var at: At? = null
         var insnListData: InsnList? = null
 
         @Throws(IllegalStateException::class)
         fun build(): AsmWriter {
-            return InjectWriter(
+            return OverwriteWriter(
                 className ?: throw IllegalStateException("className must NOT be null."),
                 methodName ?: throw IllegalStateException("methodName must NOT be null."),
-                at ?: throw IllegalStateException("at must NOT be null."),
                 insnListData ?: throw IllegalStateException("insnListData must NOT be null.")
             )
         }
