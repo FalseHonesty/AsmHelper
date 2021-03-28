56ce00ceeabfcb577dd5ee1b5633efdcb880f731
Matthew Olsson <matthewcolsson@gmail.com>
2021-03-27 20:21:00 -0700

    GeneralModificationWriter: Modify API

--- src/main/kotlin/me/falsehonesty/asmhelper/dsl/writers/GeneralModificationWriter.kt
+++ src/main/kotlin/me/falsehonesty/asmhelper/dsl/writers/GeneralModificationWriter.kt
@@ -9,17 +9,17 @@ import org.objectweb.asm.tree.MethodNode
 
 class GeneralModificationWriter(
     className: String,
-    private val modifyAction: GeneralModificationDSL.(ClassNode) -> Unit
+    private val modifyAction: GeneralModificationDSL.() -> Unit
 ) : AsmWriter(className) {
     override fun transform(classNode: ClassNode) {
-        GeneralModificationDSL().modifyAction(classNode)
+        GeneralModificationDSL(className, classNode).modifyAction()
     }
 
     override fun toString(): String {
         return "ModifyWriter{className=$className}"
     }
 
-    inner class GeneralModificationDSL {
+    class GeneralModificationDSL(val className: String, val classNode: ClassNode) {
         val public = Opcodes.ACC_PUBLIC
         val private = Opcodes.ACC_PUBLIC
         val protected = Opcodes.ACC_PUBLIC
@@ -37,48 +37,48 @@ class GeneralModificationWriter(
         val annotation = Opcodes.ACC_PUBLIC
         val enum = Opcodes.ACC_PUBLIC
 
-        fun setFieldAccess(classNode: ClassNode, fieldName: String, fieldDesc: String, visibility: Int) {
-            findField(classNode, fieldName, fieldDesc).access = visibility
+        fun setFieldAccess(fieldName: String, fieldDesc: String, visibility: Int) {
+            findField(fieldName, fieldDesc).access = visibility
         }
 
-        fun setMethodAccess(classNode: ClassNode, methodName: String, methodDesc: String, visibility: Int) {
-            findMethod(classNode, methodName, methodDesc).access = visibility
+        fun setMethodAccess(methodName: String, methodDesc: String, visibility: Int) {
+            findMethod(methodName, methodDesc).access = visibility
         }
 
-        fun makeFieldPublic(classNode: ClassNode, fieldName: String, fieldDesc: String) {
-            val field = findField(classNode, fieldName, fieldDesc)
+        fun makeFieldPublic(fieldName: String, fieldDesc: String) {
+            val field = findField(fieldName, fieldDesc)
 
             field.access = field.access or public
             field.access = field.access and (private or protected).inv()
         }
 
-        fun makeFieldNonFinal(classNode: ClassNode, fieldName: String, fieldDesc: String) {
-            findField(classNode, fieldName, fieldDesc).also {
+        fun makeFieldNonFinal(fieldName: String, fieldDesc: String) {
+            findField(fieldName, fieldDesc).also {
                 it.access = it.access and final.inv()
             }
         }
 
-        fun makeMethodPublic(classNode: ClassNode, methodName: String, methodDesc: String) {
-            val method = findMethod(classNode, methodName, methodDesc)
+        fun makeMethodPublic(methodName: String, methodDesc: String) {
+            val method = findMethod(methodName, methodDesc)
 
             method.access = method.access or public
             method.access = method.access and (private or protected).inv()
         }
 
-        fun makeMethodNonFinal(classNode: ClassNode, methodName: String, methodDesc: String) {
-            findMethod(classNode, methodName, methodDesc).also {
+        fun makeMethodNonFinal(methodName: String, methodDesc: String) {
+            findMethod(methodName, methodDesc).also {
                 it.access = it.access and final.inv()
             }
         }
 
-        fun findField(classNode: ClassNode, fieldName: String, fieldDesc: String): FieldNode {
+        fun findField(fieldName: String, fieldDesc: String): FieldNode {
             val mappedName = AsmHelper.remapper.remapFieldName(className, fieldName, fieldDesc)
 
             return classNode.fields.firstOrNull { it.name == mappedName }
                 ?: throw IllegalArgumentException("No field named $fieldName ($mappedName) found in class $className")
         }
 
-        fun findMethod(classNode: ClassNode, methodName: String, methodDesc: String): MethodNode {
+        fun findMethod(methodName: String, methodDesc: String): MethodNode {
             val mappedName = AsmHelper.remapper.remapMethodName(className, methodName, methodDesc)
             return classNode.methods.firstOrNull { it.name == mappedName }
                 ?: throw IllegalArgumentException("No field named $methodName ($mappedName) found in class $className")
